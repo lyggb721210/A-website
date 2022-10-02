c9b743cf576b76009eb3bf1ac0c52d405f8df98a
lyggb721210 <lyggb721210@163.com>
2022-10-02 05:05:21 +0000

    V 0.8.12

---
+++ index.js
@@ -0,0 +1,7 @@
+function start1() {
+    var c="https://www.baidu.com/s?ie=utf-8&wd=" + document.getElementById("search").value;
+    window.open(c,"_self");
+}
+function Settings() {
+	open("Settings.html","_self");
+}
