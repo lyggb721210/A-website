e45b83a556537c94b2053469ecd14329ec996771
lygyxr <lyggb721210@163.com>
2022-10-16 18:53:31 +0800

    123

---
+++ Nothing/hurt.py
@@ -0,0 +1,11 @@
+#coding:"utf-8"
+import turtle
+turtle.color("red")
+turtle.begin_fill()
+turtle.left(45)
+turtle.fd(100)
+turtle.circle(50,180)
+turtle.rt(90)
+turtle.circle(50,180)
+turtle.fd(100)
+turtle.end_fill()
---
+++ Nothing/newfile.py
@@ -0,0 +1,8 @@
+#coding:"utf-8"
+import turtle
+turtle.left(45)
+turtle.fd(100)
+turtle.circle(50,180)
+turtle.rt(90)
+turtle.circle(50,180)
+turtle.fd(100)
