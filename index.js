e01b4f3e4a7188a4d21f5bd911db44372b0d143e
lyggb721210 <lyggb721210@163.com>
2022-10-18 12:14:22 +0000

    123

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 window.onload=function(){
     var i=1
     while (i<3) {
-        $("#as:eq(i)").css("top","10px")
-        $("#as:eq(i)").css("","10px")
-        $("#as:eq(i)").animate({top:"-=10px",opacity:"0"},2500)
+        $("#as:eq(i)").animate({top:"-=10px",opacity:"1"},2500)
         i+1
     }
 }
