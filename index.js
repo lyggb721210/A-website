c203bec825a2c01aee9ebb93918a1c11a83d2323
lyggb721210 <lyggb721210@163.com>
2022-10-18 04:50:58 +0000

    123

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 window.onload=function(){
-    
+    var i=1
+    while (i<3) {
+        $("#as:eq(i)").css("top","10px")
+        $("#as:eq(i)").css("","10px")
+        $("#as:eq(i)").animate({top:"-=10px",opacity:"0"},2500)
+        i+1
+    }
 }
