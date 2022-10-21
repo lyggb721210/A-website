6701fb7d440d8846dfa04419dc2f5cfbfafb7453
lyggb721210 <lyggb721210@163.com>
2022-10-21 11:52:57 +0000

    123

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload=function(){
     var i=1
-    while (i<3) {
-        $("#as:eq(i)").animate({top:"-=10px",opacity:"1"},2500)
+    while (i<2) {
+        $("#as:eq(i)").animate({top:"-=10px",opacity:"-=1"},2500)
         i+1
     }
 }
