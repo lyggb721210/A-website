eafe8cb49d1f3e74e1b7df13bc8d58a2fef712b2
lyggb721210 <lyggb721210@163.com>
2022-10-24 10:33:13 +0000

    123

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-window.onload=function(){
+function hgh(){
     var i=1
     while (i<2) {
-        $("#as:eq("+i+")").animate({top:"-=10px",opacity:"-=1"},2500)
-        i+1
+        $("#as:eq("+i+")").animate({top:"-=10px",opacity:"-=1"},2500);
+        i+1;
+    }
+}
+function hghj(){
+    var a=document.getElementById("#as");
+    for(i=1,i<a.len){
+
     }
 }
